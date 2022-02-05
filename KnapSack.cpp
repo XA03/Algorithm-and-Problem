9fe8020a9c5a44ca0497c8dd39d2163c188119bb
Chu <s033390431@gmail.com>
2022-02-05 18:39:04 +0800

    Fraction KnapSack

--- KnapSack.cpp
+++ KnapSack.cpp
@@ -12,12 +12,25 @@ bool cmp(item a,item b){
     return a.rate>b.rate;
 }
 
-int Fraction(vector<item>stuff,int limitw){
+double Fraction(vector<item>stuff,int limitw){
+    double maxprofit=0,maxweight=0,index=0;
     sort(stuff.begin(),stuff.end(),cmp);
-    
-    for(int i=0;i<stuff.size();i++)cout<<stuff[i].rate<<endl;
 
-    return 0;
+    while(maxweight<limitw){
+        if(index>=stuff.size())break;
+        if(maxweight+stuff[index].weight<=limitw){
+            maxprofit+=stuff[index].profit;
+            maxweight+=stuff[index].weight;
+            index++;
+        }
+        else{
+            maxprofit+=(limitw-maxweight)*stuff[index].rate;
+            break;
+        }
+    }
+
+
+    return maxprofit;
 }
 
 int ZeroOne(){
@@ -29,10 +42,10 @@ int main(){
     struct item input;
     for(int i=0;i<3;i++){
         cin>>input.profit>>input.weight;
-        input.rate=input.profit/input.weight;
+        input.rate=(double)input.profit/input.weight;
         things.push_back(input);
     }
-    Fraction(things,10);
+    cout<<Fraction(things,8);
 
     return 0;
 }
