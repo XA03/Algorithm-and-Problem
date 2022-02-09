bb9f4aa425c90cbcd7504707bb7e1e26ca339956
Chu <s033390431@gmail.com>
2022-02-10 03:36:25 +0800

    MiniEditDistance done

--- MiniEditDistance.cpp
+++ MiniEditDistance.cpp
@@ -23,33 +23,45 @@ int miniEdit(string a,string b){
             x=dp[i-1][j]+1;
             y=dp[i][j-1]+1;
             if(a[i-1]!=b[j-1])z=dp[i-1][j-1]+1;
-            else z=dp[i-1][j-1];
-            
-            
-            
+            else{
+                z=dp[i-1][j-1];
+                trace[i][j]=3;
+                dp[i][j]=z;
+                continue;
+            }
+
             temp=min(x,y);
             temp=min(temp,z);
-            
-            cout<<i<<" "<<j<<" "<<x<<" "<<y<<" "<<" "<<z<<endl;
-
-
             if(temp==x)trace[i][j]=1;
-            else if(temp==y)trace[i][j]=2;
             else if(temp==z)trace[i][j]=0;
+            else if(temp==y)trace[i][j]=2;
+
+            //dp問題最佳解路徑並非只有一條，但我們的最佳解路徑透過coding是沒辦法輸出全部最佳解結果的
+            //若要改變路徑只需要把當最佳解不只一個時的if else statement順序改變即可 (前提是兩條路的cost是一樣的)
+            
+
+
 
             dp[i][j]=temp;
         }
     }
     
-    
     for(auto i:dp){
         for(auto j:i)cout<<j;
         cout<<endl;
     }
-    cout<<endl;
+    cout<<"\n\n";
+
     
-    
-    
+    for(auto i:trace){
+        for(auto j:i){
+            if(j==0)cout<<"s";          //replace
+            else if(j==1)cout<<"u";     //delete 
+            else if(j==2)cout<<"l";     //insert
+            else if(j==3)cout<<"m";     //match
+        }
+        cout<<endl;
+    }
     
     return dp[a.size()][b.size()];
 }
