b7cb8641eac9dde0332f96d4e88023eddccbcc39
Chu <s033390431@gmail.com>
2022-03-22 16:29:40 +0800

    Connected  Component

--- Graph/FindCC.cpp
+++ Graph/FindCC.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void BFS(vector<vector<int>>graph,int source=0){
+vector<int> BFS(vector<vector<int>>graph,int source=0){
 
     vector<int>color(graph.size(),0),
                predecessor(graph.size(),-1);
@@ -15,7 +15,6 @@ void BFS(vector<vector<int>>graph,int source=0){
             color[index]=1;
             predecessor[index]=-1;
             vertex.push(index);
-
             while(!vertex.empty()){
                 int now=vertex.front();
                 for(int j=0;j<graph.size();j++){
@@ -34,21 +33,23 @@ void BFS(vector<vector<int>>graph,int source=0){
         index=i;
     }
 
-    for(int i=0;i<predecessor.size();i++){
-
-        cout<<predecessor[i]<<" ";
-    }
-
-
+    return predecessor;
 }
 
 
 
+void collapsing(vector<int>&s,int i){   
+    int cur=i;
+    while(s[cur]!=cur){
+        if(s[cur]==-1)break;
+        cur=s[cur];
+    }
+    s[i]=cur;
+}
 
 
 int main()
 {
-
     vector<vector<int>> g = {
         {0, 5, 0, 0, 0, 3, 0},
         {5, 0, 10, 0, 1, 0, 0},
@@ -56,10 +57,30 @@ int main()
         {0, 0, 5, 0, 7, 0, 9},
         {0, 1, 0, 7, 0, 6, 2},
         {3, 0, 0, 0, 6, 0, 0},
-        {0, 4, 8, 9, 2, 0, 0}};
+        {0, 4, 8, 9, 2, 0, 0}},
+        
+        g2{{0,1,1,0,0,0,0,0},
+            {0,0,0,1,0,0,0,0},
+            {0,1,0,0,0,1,0,0},
+            {0,0,0,1,1,1,0,0},
+            {0,0,0,0,0,0,0,0},
+            {0,1,0,0,0,0,0,0},
+            {0,0,0,0,1,0,0,1},
+            {0,0,0,0,0,0,1,0}
+            };
 
-    BFS(g,0);
+    vector<int>CC=BFS(g,0),CC2=BFS(g2,0);
 
+    for(int i=0;i<CC.size();i++){
+        collapsing(CC,i);
+        cout<<CC[i]<<" ";
+    }
+    
+    cout<<endl;
+    for(int i=0;i<CC2.size();i++){
+        collapsing(CC2,i);
+        cout<<CC2[i]<<" ";
+    }
  
  
     return 0;
