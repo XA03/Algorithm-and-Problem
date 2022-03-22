8f5789471b1e781e959c0d4640525cfae43541f9
Chu <s033390431@gmail.com>
2022-03-22 14:41:54 +0800

    DFS

--- Graph/DFS.cpp
+++ Graph/DFS.cpp
@@ -2,13 +2,16 @@
 
 using namespace std;
 
-void DFSvisit(vector<int>&color,vector<int>&predecessor,vector<int>&discover,vector<int>&finish,int vertex,int &time){
+void DFSvisit(vector<vector<int>>graph,vector<int>&color,vector<int>&predecessor,vector<int>&discover,vector<int>&finish,int vertex,int &time){
     color[vertex]=1;
     discover[vertex]= ++time;
     for(int i=0;i<color.size();i++){
-        if(color[i]==0){
-            predecessor[i]=vertex;
-            DFSvisit(color,predecessor,discover,finish,i,time);
+        if(graph[vertex][i]!=0){
+            if(color[i]==0){
+                cout<<i<<" ";
+                predecessor[i]=vertex;
+                DFSvisit(graph,color,predecessor,discover,finish,i,time);
+            }
         }
     }
     color[vertex]=2;
@@ -25,21 +28,17 @@ void DFS(vector<vector<int>>graph,int source=0){
 
     for(int i=0;i<graph.size();i++){
         if(color[index]==0){
-            DFSvisit(color,predecessor,discover,finish,index,time);
+            cout<<index<<" ";
+            DFSvisit(graph,color,predecessor,discover,finish,index,time);
+            cout<<endl;
         }
         index=i;
     }
-    
-
     for(int i=0;i<color.size();i++)cout<<predecessor[i]<<" ";
     cout<<endl;
     for(int i=0;i<color.size();i++)cout<<discover[i]<<" ";
     cout<<endl;
     for(int i=0;i<color.size();i++)cout<<finish[i]<<" ";
-
-
-
-
 }
 
 
@@ -53,9 +52,25 @@ int main(){
                              {0, 0, 4, 14, 10, 0, 2, 0, 0},
                              {0, 0, 0, 0, 0, 2, 0, 1, 6},
                              {8, 11, 0, 0, 0, 0, 1, 0, 7},
-                             {0, 0, 2, 0, 0, 0, 6, 7, 0}};
+                             {0, 0, 2, 0, 0, 0, 6, 7, 0}},
+
+                             g2{{0,1,1,0,0,0,0,0},
+                                {0,0,0,1,0,0,0,0},
+                                {0,1,0,0,0,1,0,0},
+                                {0,0,0,1,1,1,0,0},
+                                {0,0,0,0,0,0,0,0},
+                                {0,1,0,0,0,0,0,0},
+                                {0,0,0,0,1,0,0,1},
+                                {0,0,0,0,0,0,1,0}
+                             };
+
+
 
     DFS(g,0);
+
+    cout<<"\n\n";
+
+    DFS(g2,0);
 
     return 0;
 }
