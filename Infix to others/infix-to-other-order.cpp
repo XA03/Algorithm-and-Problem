79f496e54fd8326720cf1a9d70dd92152d5d36f6
Chu <s033390431@gmail.com>
2022-05-25 22:46:49 +0800

    infix to other order    correctness

--- Infix to others/infix-to-other-order.cpp
+++ Infix to others/infix-to-other-order.cpp
@@ -6,10 +6,8 @@ int pri(char a){
     if(a=='+'||a=='-')return 1;
     else if(a=='*'||a=='/')return 2;
     else if(a=='^')return 3;
-    
     return 0;
 }
-
 
 
 string infix_to_postfix(string infix){
@@ -17,8 +15,9 @@ string infix_to_postfix(string infix){
     stack<char>s;
     for(int i=0;i<infix.size();i++){
         if(isalpha(infix[i]))ret+=infix[i];
-        else{   
-            if(infix[i]==')'){      //遇到括號代表還有另一個可配對的括號，把括號中的內容先印出
+        else{
+            if(infix[i]=='(')s.push(infix[i]);                      //括號為特殊優先級，直接放入operator stack
+            else if(infix[i]==')'){                                 //遇到括號代表還有另一個可配對的括號，把括號中的內容先印出
                 while(s.top()!='('){
                     ret+=s.top();
                     s.pop();
@@ -29,14 +28,14 @@ string infix_to_postfix(string infix){
                 while(!s.empty()&&pri(infix[i])<=pri(s.top())){     //如果stack是空的會直接error，因為s.top()根本沒東西可以比較
                     ret+=s.top();
                     s.pop();
-                }//while條件代表若前面碰到的operator是要先算的就得先印出符號，不然到最後一次輸出的時候順序會不對
+                }                                                   //while條件代表若前面碰到的operator是要先算的就得先印出符號，不然到最後一次輸出的時候順序會不對
                 s.push(infix[i]);
             }
         }
         
     }
 
-    while(!s.empty()){//一定會有operator沒辦法出來，因為最後一次碰到的operator沒有遇到下一次要比較的時刻
+    while(!s.empty()){                                              //一定會有operator沒辦法出來的情況，因為最後一次碰到的operator沒有遇到下一次要比較優先級的時刻
         ret+=s.top();
         s.pop();
     }
@@ -49,8 +48,9 @@ string infix_to_prefix(string infix){
     stack<char>s;
     for(int i=infix.size()-1;i>=0;i--){
         if(isalpha(infix[i]))ret+=infix[i];
-        else{   
-            if(infix[i]=='('){
+        else{
+            if(infix[i]==')')s.push(infix[i]);
+            else if(infix[i]=='('){
                 while(s.top()!=')'){
                     ret+=s.top();
                     s.pop();
@@ -58,8 +58,8 @@ string infix_to_prefix(string infix){
                 s.pop();
             }
             else{
-                while(!s.empty()&&pri(infix[i])<=pri(s.top())){     
-                    ret+=s.top();  
+                while(!s.empty() && pri(infix[i])<=pri(s.top())){
+                    ret+=s.top();
                     s.pop();
                 }
                 s.push(infix[i]);
@@ -71,8 +71,8 @@ string infix_to_prefix(string infix){
         ret+=s.top();
         s.pop();
     }
-    reverse(ret.begin(),ret.end());
 
+    reverse(ret.begin(),ret.end());
 
     return ret;
 }
@@ -81,9 +81,9 @@ string infix_to_prefix(string infix){
 
 int main(){
 
-    cout<<infix_to_postfix("a+b*c-d")<<endl;
-    cout<<infix_to_prefix("a+b*c-d");
-
+    cout<<infix_to_postfix("a*(b+c)/d")<<endl;
+    cout<<infix_to_prefix("a*(b+c)/d");
+    
 
 
 
